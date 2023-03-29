6bdfbeb0f1a46fe67db23c9a937ff3ed958f32a9
andrewsrajasekar <andrewsrajasekar@gmail.com>
2023-03-29 17:27:46 -0400

    CodeBuild Files

--- frontend/Twitchapp/src/services/api-client.ts
+++ frontend/Twitchapp/src/services/api-client.ts
@@ -1,14 +1,21 @@
 import axios from "axios";
 
+const IS_AWS_BUILD = process.env.IS_AWS_BUILD;
+
+const domain = "http://localhost:8080";
+if(!IS_AWS_BUILD){
+  
+}
+
 export const getChannels = async () => {
-  const response = await axios.get("http://localhost:8080/channels",{
+  const response = await axios.get(domain + "/channels",{
    })
    return response.data;
 }
 
 
 export const getTwitchAnalysisOfChannel = async (channel_name: any) => {
-  const response = await axios.get("http://localhost:8080/twitch_analysis",{
+  const response = await axios.get(domain + "/twitch_analysis",{
     params: {
       "channel_name": channel_name
     }
