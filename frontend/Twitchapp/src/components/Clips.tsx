45dbe33348e7737b608a92758cfa5101c2e33819
unknown <ragumadhavan168@gmail.com>
2023-03-28 22:12:41 -0400

    clips page

--- frontend/Twitchapp/src/components/Clips.tsx
+++ frontend/Twitchapp/src/components/Clips.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Stack,
+  Box,
+  Heading,
+  StackDivider,
+} from "@chakra-ui/react";
+
+const [clips, setClips] = useState([]);
+const [error, setError] = useState("");
+
+useEffect(() => {
+  apiClient
+    .get("/getclips")
+    .then((res) => setClips(res.data))
+    .catch((err) => setError(err.message));
+}, []);
 
 const Clips = () => {
-  return <div>Clips</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <Heading size="md">Generated Clips</Heading>
+      </CardHeader>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          {clips.map((clip, index) => (
+            <Box>
+              <h4>clip {index + 1}</h4>
+              <button></button>
+            </Box>
+          ))}
+        </Stack>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default Clips;
