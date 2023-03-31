a2edd0048283996e1df162935045b0b9c5f33f0c
andrewsrajasekar <andrewsrajasekar@gmail.com>
2023-03-31 18:57:48 -0400

    Backend url change

--- frontend/Twitchapp/src/services/api-client.ts
+++ frontend/Twitchapp/src/services/api-client.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-// const IS_AWS_BUILD = process.env.IS_AWS_BUILD;
+const IS_AWS_BUILD = process.env.IS_AWS_BUILD;
 
-const domain = "http://localhost:8080";
-// if(!IS_AWS_BUILD){
-  
-// }
+let domain = "http://localhost:8080";
+if(!IS_AWS_BUILD){
+  domain = "http://andrewstwitch.us-east-1.elasticbeanstalk.com"; 
+}
 
 export const getChannels = async () => {
+  console.log(IS_AWS_BUILD);
+  console.log(domain);
   const response = await axios.get(domain + "/channels",{
    })
    return response.data;
