44c4a555b1db322368a656f6f417aec40320d33c
andrewsrajasekar <andrewsrajasekar@gmail.com>
2023-04-02 20:26:27 -0400

    Changes to Build

--- frontend/Twitchapp/src/services/api-client.ts
+++ frontend/Twitchapp/src/services/api-client.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const IS_AWS_BUILD = false;
-//process.env.IS_AWS_BUILD;
+const IS_AWS_BUILD = import.meta.env.IS_AWS_BUILD;
 
 let domain = "http://localhost:8080";
 if(!IS_AWS_BUILD){
-  domain = "https://qa3hncyg0j.execute-api.us-east-1.amazonaws.com/dev/"; 
+  domain = "https://qa3hncyg0j.execute-api.us-east-1.amazonaws.com/dev"; 
 }
 
 export const getChannels = async () => {
   console.log(IS_AWS_BUILD);
+  console.log(import.meta.env);
   console.log(domain);
   const response = await axios.get(domain + "/channels",{
    })
