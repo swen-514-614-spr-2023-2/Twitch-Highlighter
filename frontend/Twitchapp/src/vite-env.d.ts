1fe6a24e3cfe5060921f2f2b8370032da54625d2
andrewsrajasekar <andrewsrajasekar@gmail.com>
2023-04-26 15:38:13 -0400

    Issue Fix

--- frontend/Twitchapp/src/vite-env.d.ts
+++ frontend/Twitchapp/src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-interface ImportMetaEnv {
-    VITE_AWS_REGION: "us-east-1",
-    // more env variables...
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
